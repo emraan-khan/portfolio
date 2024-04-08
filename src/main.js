7857f855a6886b132efbde75e687db9d2275c1ee
Imran Khan <imrankhan772244@gmail.com>
2024-04-08 14:53:46 +0530

    new commit

--- src/main.js
+++ src/main.js
@@ -4,4 +4,4 @@ import '../styles/components/header.css'
 import '../styles/components/hero.css'
 import '../styles/components/about.css'
 import '../styles/components/expreince.css'
-import "../styles/utils.css"
+import '../styles/utils.css'
