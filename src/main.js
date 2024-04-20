037202a77374ca8d78aa99bc19883020b75849d6
Imran Khan <imrankhan772244@gmail.com>
2024-04-20 20:52:35 +0530

    mobile status update

--- src/main.js
+++ src/main.js
@@ -5,4 +5,13 @@ import '../styles/components/hero.css'
 import '../styles/components/about.css'
 import '../styles/components/expreince.css'
 import '../styles/components/work.css'
+import '../styles/components/contact.css'
+import '../styles/components/footer.css'
+import '../styles/components/mobile-nav.css'
 import '../styles/utils.css'
+
+// import fucntion from another js files
+
+import mobileNav from './utils/mobile-nav'
+
+mobileNav();
---
+++ src/utils/mobile-nav.js
@@ -0,0 +1,19 @@
+const mobileNav = () => {
+    const headerBtn = document.querySelector('.header__bars');
+    const mobileNav = document.querySelector('.mobile-nav');
+    const mobileLinks =  document.querySelectorAll('.mobile-nav__link')
+    let isMobileNavOpen = false;
+
+    headerBtn.addEventListener('click',()=>{
+        isMobileNavOpen = !isMobileNavOpen;
+        if(isMobileNavOpen){
+        mobileNav.style.display = 'flex';
+        document.body.style.overflowY = 'hidden';
+        }else {
+            mobileNav.style.display = 'none';
+        document.body.style.overflowY = 'auto';
+        }
+    })
+}
+
+export default mobileNav;
