5d1be60cbc4b1e8012c787d9690c7af71e93fb69
Imran Khan <imrankhan772244@gmail.com>
2024-04-22 23:47:13 +0530

    mob nav update

--- src/utils/dark-mode.js
+++ src/utils/dark-mode.js
@@ -2,10 +2,22 @@ const darkMode = () => {
 
     const themeToggleBtns = document.querySelectorAll('#theme-toggle');
 
+    const theme = localStorage.getItem('theme');
+
+    theme && document.body.classList.add(theme);
+
+    const handleToggle = () => {
+        document.body.classList.toggle('light-mode');
+        if (document.body.classList.contains('light-mode')) {
+            localStorage.setItem('theme', 'light-mode');
+        } else {
+            localStorage.removeItem('theme');
+            document.body.removeAttribute('class')
+        }
+    }
+
     themeToggleBtns.forEach((btn) => {
-        btn.addEventListener('click', () => {
-            document.body.classList.toggle('light-mode');
-        })
+        btn.addEventListener('click', handleToggle)
     })
 }
 
