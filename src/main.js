7c002bc67bb7b9923beff26ed7553579b39d7bd2
Imran Khan <imrankhan772244@gmail.com>
2024-04-26 20:46:12 +0530

    almost completed

--- src/main.js
+++ src/main.js
@@ -14,7 +14,10 @@ import '../styles/utils.css'
 
 import mobileNav from './utils/mobile-nav'
 import darkMode from './utils/dark-mode'
+import lazyLoading from './utils/lazy-loading'
 
 mobileNav();
 
 darkMode();
+
+lazyLoading();
